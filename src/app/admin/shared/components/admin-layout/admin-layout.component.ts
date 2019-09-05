d7cb459ad194dd165d2f97516632a8c45de8b50b
Vladimir Ilin <mark09072010@gmail.com>
2019-09-05 23:35:22 +0300

    init

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  logout(event: Event) {
+    event.preventDefault();
+    this.router.navigate(['/admin' ,'login']);
   }
 
 }
